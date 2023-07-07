67a4fc2b7ed85ef2cb90e69502656fe8ce123d9d
arjungarg <30733139+arjungarg@users.noreply.github.com>
2023-07-07 18:04:09 +0530

    Update WelcomeController.java

--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -25,6 +25,6 @@ class WelcomeController {
 
     @GetMapping("/")
     public String welcome() {
-        return "Jai Jai Shri Radhey";
+        return "Jai Jai Shri Radhey Welcome ji Vishai Tyagi and Sumit choudhary";
     }
 }
